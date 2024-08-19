5bdb98141870a6c8e3c3fe56cb75a83f1c1bd1d5
Vinicius Eduardo <111357753+viivinii@users.noreply.github.com>
2024-08-19 09:49:33 -0300

    segunda aula

--- script.js
+++ script.js
@@ -32,12 +32,18 @@ const perguntas = [
         {
           texto:
             "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-          afirmacao: "afirmação",
+          afirmacao: [
+          "afirmação",
+          ""
+         ]
         },
         {
           texto:
             "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-          afirmacao: "afirmação",
+          afirmacao: [
+          "afirmação",
+          ""
+         ]
         },
       ],
     },
@@ -48,12 +54,18 @@ const perguntas = [
         {
           texto:
             "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-          afirmacao: "afirmação",
+          afirmacao: [
+          "afirmação",
+          ""
+         ]
         },
         {
           texto:
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-          afirmacao: "afirmação",
+          afirmacao: [
+          "afirmação",
+          ""
+         ]
         },
       ],
     },
@@ -64,11 +76,17 @@ const perguntas = [
         {
           texto:
             "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-          afirmacao: "afirmação",
+          afirmacao: [ 
+          "afirmação",
+          ""
+         ]
         },
         {
           texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-          afirmacao: "afirmação",
+          afirmacao: [ 
+           "afirmação",
+           ""
+         ]
         },
       ],
     },
